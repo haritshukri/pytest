74877d929c9149f15ecbd30957f532c8c2f7179d
haritshukri <mharitshukri@gmail.com>
2023-12-05 21:45:32 +0800

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,29 @@
+#ini ada comment yang korang boleh baca
+"""
+macam ini pun
+boleh jadi comment
+"""
+
+x = 5 #dalam python no need to declare variable
+y = "baba" #kalau nak tulis string kena ada ""
+
+print(x)
+print(y)
+
+a = str(3) #specify untuk str akan jadi 3
+b = int(3) #specify untuk int akan jadi 3
+c = float(3) #specify untuk float akan jadi 3.0
+
+print(a,b,c) #nak print sebaris boleh buat macam ni
+print(type(a),type(b),type(c)) #ini adalah cara untuk mengenal pasti data type
+
+x = 'baba'
+#adalah sama dengan
+x = "baba"
+
+#akan tetapi
+g = 4
+#tidak bersamaan dengan
+G = 'lekir'
+#python dia case sensitive huruf besar tak akan overwrite huruf kecil
+
