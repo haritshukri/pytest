f07b5536e958ac2e4d1a28ba41d70b8a0a3a9445
haritshukri <mharitshukri@gmail.com>
2023-12-06 19:51:00 +0800

    'wstimer.py'

---
+++ wstimer.py
@@ -0,0 +1,11 @@
+import pywhatkit
+
+pywhatkit.sendwhatmsg(
+    '+601139394022', #no ws yang kita nak hantar
+    'woi dapat tak mesej ni', #mesej nak dihantar ke nombor 
+    20, #jam dalam 24 hour format
+    00, #minute 
+    10, #10 saat akan digunakan untuk buka ws
+    True, #untuk close ws lepas dah guna
+    3, #saat untuk close browser
+)
