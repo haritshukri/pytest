3e7ec1732fbab983cc38c15cfe607cf6922f76f6
haritshukri <mharitshukri@gmail.com>
2023-12-06 20:11:47 +0800

    'finally'

--- test2.py
+++ test2.py
@@ -4,7 +4,7 @@ pywhatkit.sendwhatmsg(
     '+601139394022', #no ws yang kita nak hantar
     'woi dapat tak mesej ni', #mesej nak dihantar ke nombor 
     20, #jam dalam 24 hour format
-    2, #minute 
+    5, #minute 
     10, #10 saat akan digunakan untuk buka ws
     True, #untuk close ws lepas dah guna
     3, #saat untuk close browser
--- wstimer2.py
+++ wstimer2.py
@@ -4,8 +4,12 @@ pywhatkit.sendwhatmsg(
     '+601139394022', #no ws yang kita nak hantar
     'woi dapat tak mesej ni', #mesej nak dihantar ke nombor 
     20, #jam dalam 24 hour format
-    2, #minute 
-    10, #10 saat akan digunakan untuk buka ws
+    10, #minute 
+    20, #20 saat akan digunakan untuk buka ws
     True, #untuk close ws lepas dah guna
     3, #saat untuk close browser
+
+    """
+    ini kali jadi guys, dia buka chrome 20 saat awal then send mesej, after that dia terus close tab
+    """
 )
